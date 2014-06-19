8f797be76a42d86b88a98dd3d6f190fd57d0a89a
Maarten van Beek <maartenvanbeek@gmail.com>
2014-06-19 14:14:54 +0200

    Fixed Scene bug that gave objects in wrong order

--- Scene.cpp
+++ Scene.cpp
@@ -8,19 +8,18 @@ Scene::Scene(){}
 
 Hit Scene::intersect(Vec3Df origin, Vec3Df dest){
 	Hit h = Hit(0, Vec3Df(0,0,0),Vec3Df(0,0,0),Material());
-	float current_length = 999;
+	float current_length = 0;
 
 	for(int i = 0; i < Objects.size(); i++){
 		Hit obj_h = Objects[i]->intersect(origin, dest);
 
 		if(obj_h.isHit){
-			Vec3Df length_vector = h.hitPoint-origin;
+			Vec3Df length_vector = obj_h.hitPoint-origin;
 			float length = Vec3Df::dotProduct(length_vector, length_vector);
 
-			if(length < current_length){
+			if(!h.isHit || length < current_length){
 				current_length = length;
 				h = obj_h;
-
 			}
 		}
 	}
