f73fc6edf79f154487131264a862b5db427fd1fa
Maarten van Beek <maartenvanbeek@gmail.com>
2014-06-21 16:09:12 +0200

    Fixed felix bug

--- Ray.cpp
+++ Ray.cpp
@@ -1,9 +1,5 @@
 #include "Ray.h"
 
-Ray::Ray(Vec3Df _origin, Vec3Df _dest, Vec3Df _colour, RayType _type){
-	Ray(_origin, _dest, _colour, _type, Hit());
-}
-
 Ray::Ray(Vec3Df _origin, Vec3Df _dest, Vec3Df _colour, RayType _type, Hit _previous_hit){
 	origin = _origin;
 	dest = _dest;
@@ -12,12 +8,10 @@ Ray::Ray(Vec3Df _origin, Vec3Df _dest, Vec3Df _colour, RayType _type, Hit _previ
 	previous_hit = _previous_hit;
 }
 
-Ray Ray::reflectionVector(Hit h){
+Ray Ray::reflectionRay(Hit h){
 	Vec3Df relative_origin = origin-h.hitPoint;
 	Vec3Df reflection_vector = relative_origin - (2*(Vec3Df::dotProduct(relative_origin, h.normal))*h.normal);
 	reflection_vector += h.hitPoint;
 
-	RayType reflect_type = REFLECTION_RAY;
-
-	return Ray(h.hitPoint, reflection_vector, colour, reflect_type);
+	return Ray(h.hitPoint, reflection_vector, colour, SECONDARY_RAY, h);
 }
