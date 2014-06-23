9a6c3e3ad6a0f88c27f5d83223852786f4cc74cc
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-06-23 06:18:38 +0200

    According to the given link the h.normal should be the refrac_index (lowercase N)

--- Ray.cpp
+++ Ray.cpp
@@ -25,7 +25,7 @@ Ray Ray::refractionRay(Hit h, float medium1, float medium2){
 	float refrac_index = medium1/medium2;
 	float c2 = sqrt( 1 - pow(refrac_index, 2) * (1 - pow(c,2)) );
 
-	Vec3Df refrac_ray = (h.normal * (origin-h.hitPoint)) + h.normal*(refrac_index * c - c2);
+	Vec3Df refrac_ray = (refrac_index * (origin-h.hitPoint)) + h.normal*(refrac_index * c - c2);
 
 	return Ray(h.hitPoint, refrac_ray, colour, SECONDARY_RAY, h);
 }
