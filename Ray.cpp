2e84a8ad568d4a7a55008173a52ee6cd21dd8228
Calvin Wong Loi Sing <calvin@wlsing.nl>
2014-06-23 06:23:34 +0200

    Fixed some bugs(?) in the refraction code. It didnt correspond to the given resource

--- Ray.cpp
+++ Ray.cpp
@@ -18,14 +18,14 @@ Ray Ray::reflectionRay(Hit h){
 
 //https://www.cs.unc.edu/~rademach/xroads-RT/RTarticle.html
 Ray Ray::refractionRay(Hit h, float medium1, float medium2){
-
-
-	float c = -Vec3Df::dotProduct( h.normal, origin-h.hitPoint);
-
+	Vec3Df relative_origin = origin - h.hitPoint;
+	Vec3Df c1 = -Vec3Df::dotProduct(h.normal, relative_origin);
+	
 	float refrac_index = medium1/medium2;
+	// Snells law
 	float c2 = sqrt( 1 - pow(refrac_index, 2) * (1 - pow(c,2)) );
 
-	Vec3Df refrac_ray = (refrac_index * (origin-h.hitPoint)) + h.normal*(refrac_index * c - c2);
+	Vec3Df refrac_ray = (refrac_index * relative_origin) + (refrac_index * c1 - c2) * h.normal;
 
 	return Ray(h.hitPoint, refrac_ray, colour, SECONDARY_RAY, h);
 }
